a2c1aa8813b3d9664b60f275315899eb7b8d67d3
rodrigo-ogava <rodrigo.ogava@gmail.com>
2018-01-16 17:23:40 -0300

    implementação da variável controle e outras mudanças dejogabilidade

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
-let w = 1000;
+let w = 900;
 let h = 600;
 let grad = 20;
 let ace = 0.35;
-let pot = 0;
+let vel_ini = 5;
+let controle = false;
 
 function setup() {
     createCanvas(w, h);
+	ellipseMode(CENTER);
+	rectMode(CENTER);
 	b = new Bola();
 	r1 = new Ret(grad);
 	r2 = new Ret(w-grad);
@@ -14,31 +17,40 @@ function setup() {
 function draw() {
     background(51);
     stroke(255);
-    strokeWeight(3);
+    strokeWeight(4);
     line(w/2,0,w/2,h-1);
 	
-	textSize(30);
+	textSize(36);
 	strokeWeight(1);
-	text(("0"+pot).slice(-2),w/5,2*grad);
+	fill(255);
+	text(("0"+b.pont).slice(-2),w/4.5,2*grad);
 	
-	r1.move();
-	r2.move();
+	b.update();
+	
+	if(b.x < 0 || b.x > w-1) {
+		b = new Bola();
+	}
+	
 	if(b.toca(r1)) {
-		if(abs(b.velx) < 16) {
-			b.velx -= ace;
-		}
-		b.velx = b.velx * -1;
-		b.x += b.velx;
-		pot += 1;
+		if(controle) {
+			if(abs(b.velx) < 20) {
+				b.velx -= ace;
+			}
+			b.velx = b.velx * -1;
+			b.x += 0.5*b.velx;
+			b.pont += 1;
+			controle = false;
+		} 
 	}
 	if(b.toca(r2)) {
-		if(abs(b.velx) < 14) {
-			b.velx += ace;
+		if(!controle) {
+			b.velx = b.velx * -1;
+			b.x += 0.5*b.velx;
+			controle = true;
 		}
-		b.velx = b.velx * -1;
-		b.x += b.velx;
 	}
-	b.update();
+	r1.move();
+	r2.move();
 	r1.show();
 	r2.show();
 	b.show();
@@ -46,20 +58,21 @@ function draw() {
 
 function Ret(x) {
 	this.x = x;
-	this.y = 200;
-	this.w = 7;
-	this.h = 90;
+	this.y = h/2;
+	this.w = 8;
+	this.h = h/6;
+	
 	this.show = function() {
-		rectMode(CENTER);
 		noStroke();
-		rect(this.x,this.y,this.w,this.h);
 		fill(255);
+		rect(this.x,this.y,this.w,this.h);
 	}
+	
 	this.move = function() {
-		if(keyIsDown(UP_ARROW) && this.y - this.h/2 > 5){
-			this.y -= 10;
-		} else if (keyIsDown(DOWN_ARROW) && this.y + this.h/2 < h-5){
-			this.y += 10;
+		if(keyIsDown(UP_ARROW) && this.y - this.h/2 > 7){
+			this.y -= 12;
+		} else if (keyIsDown(DOWN_ARROW) && this.y + this.h/2 < h-8){
+			this.y += 12;
 		}
 	}
 }
@@ -68,31 +81,27 @@ function Bola() {
 	this.x = w/2;
 	this.y = h/2;
 	this.d = 18;
-	this.velx = 5;
-	this.vely = random(-3,3);
+	this.velx = vel_ini;
+	this.vely = random(-4,4);
+	this.pont = 0;
 	
 	this.show = function() {
-		ellipseMode(CENTER);
 		noStroke();
-    	ellipse(this.x, this.y, this.d, this.d);
 		fill(255);
+    	ellipse(this.x, this.y, this.d, this.d);
 	}
+	
 	this.update = function() {
 		this.x = this.x + this.velx;
 		this.y = this.y + this.vely;
 		if(this.y + this.d/2 > h-1 || this.y - this.d/2 < 0) {
 			this.vely = this.vely * -1;
 		}
-		if(this.x < grad || this.x > w-grad-1) {
-			this.x = w/2;
-			this.y = h/2;
-			this.velx = 3;
-			this.vely = random(-3,3);
-			pot = 0;
-		}
 	}
+	
 	this.toca = function(ret) {
-		if((abs(ret.x - this.x)<=(ret.w + this.d)/2)&&(this.y + this.d/4 >= ret.y - ret.h/2)&&(this.y - this.d/4 <= ret.y + ret.h/2)) {
+		if(abs(ret.x - this.x) <= (ret.w + this.d)/2 && abs(this.y - ret.y) <= (ret.h + 1.3*this.d)/2) {
+		//(this.y + this.d/4 >= ret.y - ret.h/2)&&(this.y - this.d/4 <= ret.y + ret.h/2)
 			return true;
 		} else {
 			return false;
